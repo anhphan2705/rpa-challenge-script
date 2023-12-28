87655ca0facd4c3221921457afc27a64a399888f
anhphan2705 <anhphanvt2705@gmail.com>
2023-12-28 00:47:33 -0500

    bug 'use init() before using url()'

--- rpa_script.py
+++ rpa_script.py
@@ -4,23 +4,23 @@ import pandas as pd
 data_frame = pd.read_excel("./Robotic-Process-Automation/rpa-challenge-script/data/challenge.xlsx")
 
 # Start the tagUI process
-r.init(visual_automation=True, chrome_browser=True, turbo_mode=True)
+r.init(turbo_mode=True, chrome_browser=True)
 
 # Open the website
-r.url("https://rpachallenge.com/")
+r.url('https://rpachallenge.com/')
 r.wait()
 
 # Click on Start button
 r.click('//button[text()="Start"]')
 
 for index, row in data_frame.iterrows():
-    r.type('//input[@ng-reflect-name="labelFirstName"]', row("First Name"))
-    r.type('//input[@ng-reflect-name="labelLastName"]', row("Last Name"))
-    r.type('//input[@ng-reflect-name="labelCompanyName"]', row("Company Name"))
-    r.type('//input[@ng-reflect-name="labelRole"]', row("Role In Company"))
-    r.type('//input[@ng-reflect-name="labelAddress"]', row("Address"))
-    r.type('//input[@ng-reflect-name="labelEmail"]', row("Email"))
-    r.type('//input[@ng-reflect-name="labelPhone"]', str(row("Phone Number")))
+    r.type('//input[@ng-reflect-name="labelFirstName"]', row["First Name"])
+    r.type('//input[@ng-reflect-name="labelLastName"]', row["Last Name "])
+    r.type('//input[@ng-reflect-name="labelCompanyName"]', row["Company Name"])
+    r.type('//input[@ng-reflect-name="labelRole"]', row["Role in Company"])
+    r.type('//input[@ng-reflect-name="labelAddress"]', row["Address"])
+    r.type('//input[@ng-reflect-name="labelEmail"]', row["Email"])
+    r.type('//input[@ng-reflect-name="labelPhone"]', str(row["Phone Number"]))
     r.click('//input[@value="Submit"]')
 
 # Screenshot of Webpage
