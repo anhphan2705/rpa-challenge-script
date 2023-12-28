778c9306cb6b158c1df2d5c70f2126ca34fafc8e
anhphan2705 <anhphanvt2705@gmail.com>
2023-12-28 00:34:16 -0500

    update progress

--- rpa_script.py
+++ rpa_script.py
@@ -1,10 +1,10 @@
 import rpa as r
 import pandas as pd
 
-data_frame = pd.read_excel("./data/challenge.xlsx")
+data_frame = pd.read_excel("./Robotic-Process-Automation/rpa-challenge-script/data/challenge.xlsx")
 
 # Start the tagUI process
-r.init()
+r.init(visual_automation=True, chrome_browser=True, turbo_mode=True)
 
 # Open the website
 r.url("https://rpachallenge.com/")
@@ -22,6 +22,9 @@ for index, row in data_frame.iterrows():
     r.type('//input[@ng-reflect-name="labelEmail"]', row("Email"))
     r.type('//input[@ng-reflect-name="labelPhone"]', str(row("Phone Number")))
     r.click('//input[@value="Submit"]')
-    
+
 # Screenshot of Webpage
-r.snap('/html/body/app-root/div[2]', 'result.png')
+r.snap("/html/body/app-root/div[2]", "result.png")
+
+# Stop the tagUI process
+r.close()
